6b8df2ab367bc9e444466aeb4027f3903ef02d94
Stanimir Kanchev <stani6996@gmai.com>
2018-05-13 08:51:38 +0300

    Updating

--- src/screens/startPage/StartPage.js
+++ src/screens/startPage/StartPage.js
@@ -49,8 +49,18 @@ class StartPage extends Component {
 
   renderPage2 () {
     return (
-      <div>
-        Page 2
+      <div className='page1'>
+      <div style={{ width: 1189, height: "100%", position: 'relative', margin: 'auto' }}>
+      <img alt='' className='backgroundfootballhd8' src='https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af6e99c994d70000c325ad3/img/page 1backgroundfootballhd8.png' anima-src='https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af6e99c994d70000c325ad3/img/page 1backgroundfootballhd8.png' />
+        <img alt='' class="oval2" src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2oval 2.png" anima-src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2oval 2.png"/>
+        <img alt='' class="now-enter-your-guest" src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2now enter your guest.png" anima-src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2now enter your guest.png"/>
+        <img alt='' class="rectangle2" src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af704e783b433000c08d4cd/img/page 1rectangle 2.png" anima-src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af704e783b433000c08d4cd/img/page 1rectangle 2.png"/>
+        <img alt='' class="group2" src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2group 2.png" anima-src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2group 2.png"/>
+        <img alt='' class="footballspace" src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af704e783b433000c08d4cd/img/page 1footballspace.png" anima-src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af704e783b433000c08d4cd/img/page 1footballspace.png"/>
+        <img alt='' class="bitmap" src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af6e99c994d70000c325ad3/img/page 1bitmap.png" anima-src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af6e99c994d70000c325ad3/img/page 1bitmap.png"/>
+        <img alt='' class="icons8home26" src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2icons8home26.png" anima-src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2icons8home26.png"/>
+        <img alt='' class="icons8contactsfilled50" src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2icons8contactsfilled50.png" anima-src="https://anima-uploads.s3.amazonaws.com/5af6e91783b433000c08d471/5af6e99b994d70000b8b9d26/5af737e6006ce3000b613e99/img/page 2icons8contactsfilled50.png"/>
+        </div>
       </div>
     )
   }
